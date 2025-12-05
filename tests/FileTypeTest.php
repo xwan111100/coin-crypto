00e9b99a624b3f9d3713d4c9b93f87ec74d9c7ae
xwan111100 <telocaspey6@gmail.com>
2025-12-05 07:12:12 +0700

    menambahkan

---
+++ tests/FileTypeTest.php
@@ -0,0 +1,41 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class FileTypeTest extends TestCase
+{
+    private $projectFiles = [
+        'index.php',
+        'gallery.php'
+    ];
+
+    public function test_files_exist()
+    {
+        foreach ($this->projectFiles as $file) {
+            $this->assertFileExists($file, "File $file tidak ditemukan!");
+        }
+    }
+
+    public function test_php_files_contain_php_code()
+    {
+        foreach ($this->projectFiles as $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                $content = file_get_contents($file);
+                $this->assertStringContainsString('<?php', $content, "File $file tidak mengandung kode PHP!");
+            }
+        }
+    }
+
+    public function test_html_files_contain_html_tags()
+    {
+        foreach ($this->projectFiles as $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                $content = file_get_contents($file);
+                $this->assertMatchesRegularExpression(
+                    '/<html>|<head>|<body>/i',
+                    $content,
+                    "File $file bukan HTML yang valid!"
+                );
+            }
+        }
+    }
+}
